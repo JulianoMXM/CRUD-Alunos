87224a465381f8164ffcbcb3b037f66811b2c241
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-08 20:49:27 -0300

    refact: Ajusta para usar crase ao invés de aspas simples pra permitir o uso das constantes de autenticação

--- CRUD.js
+++ CRUD.js
@@ -20,7 +20,7 @@ const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 
 mongoose
-    .connect('mongodb+srv://${DB_USER}:${DB_PASSWORD}@projetounect.h56t2wl.mongodb.net/?appName=ProjetoUnect')
+    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@projetounect.h56t2wl.mongodb.net/?appName=ProjetoUnect`)
     .then(() => {
         console.log('Conectado')
         app.listen(3000)
