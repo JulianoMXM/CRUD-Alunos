eba739367afbdd54d8829dc7b427939fdb2ebf9d
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-10 00:34:49 -0300

    refact: Atualização de login

--- CRUD.js
+++ CRUD.js
@@ -20,7 +20,8 @@ const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 
 mongoose
-    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@projetounect.h56t2wl.mongodb.net/?appName=ProjetoUnect`)
+    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@projetosunect.wtzr5ke.mongodb.net/`
+)
     .then(() => {
         console.log('Conectado')
         app.listen(3000)
